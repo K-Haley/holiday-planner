2c50cbeb3206c623e37e096e1b712b616ca1c6e7
Kevin Haley <khaley319@gmail.com>
2018-12-19 14:17:15 -0800

    got chat with names

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -7,6 +7,8 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
+
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'user': request.session['name']
     })
